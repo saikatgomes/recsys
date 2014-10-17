3269def118deb44b68757dd858b89db1914803dc
saikatgomes <saikatgomes@gmail.com>
2014-10-17 18:40:16 -0500

    script to download tweet data

---
+++ code/get_movie_tweeting_data.sh
@@ -0,0 +1,16 @@
+mkdir -p ../data
+mkdir -p ../data/tweet
+mkdir -p ../data/tweet/backup
+
+cd ../data/tweet
+
+cp --backup=numbered *.dat backup/.
+
+wget -N https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/movies.dat
+wget -N https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/users.dat
+wget -N https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/ratings.dat
+
+ls -lrth
+
+cd ../code
+
