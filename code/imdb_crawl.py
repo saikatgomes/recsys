e2c2a9324b9ad4702aed6051e40d69a5931a2be5
saikatgomes <saikatgomes@gmail.com>
2014-10-22 21:08:53 -0500

    more IMDB data

--- code/imdb_crawl.py
+++ code/imdb_crawl.py
@@ -29,10 +29,10 @@ def get_value(tree,scrap_str,err_file,id,att_name):
         aValue=dom_obj[0]
     else:
         err_file.write(getTime()+"ID:"+id+"|MSG:No_"+att_name+"_Found\n")
-    print(getTime()+"\t\t"+att_name+": "+aValue)
+    print(getTime()+"\t\t\t"+att_name+": "+aValue)
     return aValue
 
-def get_value_list(tree,scrap_str,err_file,id,att_name):
+def get_value_list(tree,scrap_str,err_file,id,att_name,PRINT=1):
     aValue=[]
     dom_obj=tree.xpath(scrap_str)
     if len(dom_obj)>0:
@@ -40,9 +40,24 @@ def get_value_list(tree,scrap_str,err_file,id,att_name):
             aValue.append(d)
     else:
         err_file.write(getTime()+"ID:"+id+"|MSG:No_"+att_name+"_Found\n")
-    msg=", ".join(aValue)
-    print(getTime()+"\t\t"+att_name+": "+msg)
+    if PRINT==1:
+        msg=", ".join(aValue)
+        print(getTime()+"\t\t\t"+att_name+": "+msg)
     return aValue
+
+def get_simple_value(obj,scrapy_str):
+    value=""
+    d=obj.xpath(scrapy_str)
+    if len(d)>2:
+        value=d[1].strip(" \n")
+    return value
+
+def get_link_value(obj,scrapy_str):
+    value=""
+    d=obj.xpath(scrapy_str)
+    if len(d)>0:
+        value=d[0]
+    return value
 
 def get_movie_url():
     global MOVIES, M_COUNT
@@ -53,30 +68,63 @@ def get_movie_url():
         with open(movie_fName,'r') as f:
             for line in f:
                 M_COUNT=M_COUNT+1
+                # Parse Tweet Data
                 myData=re.split('::',line)
                 id=myData[0]
                 title=myData[1][:len(myData[1])-7]
                 year=myData[1][len(myData[1])-5:len(myData[1])-1]
                 cat=re.split('\|',myData[2][:len(myData[2])-1])
+                # URLs
                 find_url=BASE_URL+"/find?ref_=nv_sr_fn&q="+title.replace(' ','+')+"&s=tt"
                 find_url2=BASE_URL+"/find?q="+title.replace(' ','+')+"&s=tt"
                 cast_url=BASE_URL+"/title/tt"+id+"/fullcredits?ref_=tt_cl_sm#cast"
                 main_url=BASE_URL+"/title/tt"+id+"/"
                 main_page=requests.get(main_url)
-                print(getTime()+"Crawling url: "+main_url)
-                print(getTime()+"\t\tID: "+id)
-                print(getTime()+"\t\tTitle: "+title)
-                print(getTime()+"\t\tYear: "+year)
-                msg=", ".join(cat)
-                print(getTime()+"\t\tGenre: "+msg)
                 tree = html.fromstring(main_page.text)
+                print(getTime()+"Crawling url: "+main_url)
+                print(getTime()+"\t\tTweet Data:")
+                print(getTime()+"\t\t\tID: "+id)
+                print(getTime()+"\t\t\tTitle: "+title)
+                print(getTime()+"\t\t\tYear: "+year)
+                msg=", ".join(cat)
+                print(getTime()+"\t\t\tGenre: "+msg)
                 # SCRAP FOR DATA
+                print(getTime()+"\t\tIMDB Data:")
                 rating = get_value(tree,"//span[@itemprop='ratingValue']/text()",err_file,id,"Ratings")
                 director = get_value_list(tree,"//div[@itemprop='director']/a/span[@itemprop='name']/text()",err_file,id,"Director")
                 stars=get_value_list(tree,"//div[@itemprop='actors']/a/span[@itemprop='name']/text()",err_file,id,"Actors")
                 recs=get_value_list(tree,"//div[starts-with(@class,'rec_item')]/@data-tconst",err_file,id,"Recs")
                 mpaa=get_value(tree,"//span[@itemprop='contentRating']/text()",err_file,id,"MPAA")
                 keywords=get_value_list(tree,"//div[@id='titleStoryLine']/div[@itemprop='keywords']/a/span[@itemprop='keywords']/text()",err_file,id,"Keywords")
+                lang=""
+                gross=""
+                runtime=""
+                color=""
+                aka=""
+                budget=""
+                details=get_value_list(tree,"//div[@id='titleDetails']/div[@class='txt-block']",err_file,id,"Detials",0)
+                for d in details:
+                    d_name=d.xpath("h4/text()")
+                    if len(d_name)>0:
+                        name=d_name[0]
+                        if name=="Color:":
+                            color=get_link_value(d,"a/text()")
+                        elif name=="Budget:":
+                            budget=get_simple_value(d,"text()")
+                        elif name=="Language:":
+                            lang=get_link_value(d,"a/text()")
+                        elif name=="Gross:":
+                            gross=get_simple_value(d,"text()")
+                        elif name=="Runtime:":
+                            runtime=get_link_value(d,"time/text()")
+                        elif name=="Also Known As:":
+                            aka=get_simple_value(d,"text()")
+                print(getTime()+"\t\t\tLanguage: "+lang)
+                print(getTime()+"\t\t\tColor: "+color)
+                print(getTime()+"\t\t\tRuntime: "+runtime)
+                print(getTime()+"\t\t\tBudget: "+budget)
+                print(getTime()+"\t\t\tGross: "+gross)
+                print(getTime()+"\t\t\tKnow As: "+aka)
                 # STORE!
                 MOVIES.append({'id':id,'title_tweet':title,'year_tweet':year,'genre_tweet':cat,'find_url':find_url,'find_url2':find_url2,'cast_url':cast_url,'url':main_url,'rating_imdb':rating})
             movie_json=OUT_DIR+"/movies.json"
