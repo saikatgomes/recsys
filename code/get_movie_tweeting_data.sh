f6f00f0647f53a3b44a6db0df3e2044fedd98277
saikatgomes <saikatgomes@gmail.com>
2014-10-22 23:13:20 -0500

    download and splitfiles

--- code/get_movie_tweeting_data.sh
+++ code/get_movie_tweeting_data.sh
@@ -3,10 +3,10 @@ mkdir -p ../data/tweet
 mkdir -p ../data/tweet/backup
 mkdir -p ../data/imdb
 mkdir -p ../data/imdb/parts
+mkdir -p ../data/imdb/parts/backup
 mkdir -p ../data/db
 
 cd ../data/tweet
-
 cp --backup=numbered *.dat backup/.
 
 wget -N https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/movies.dat
@@ -15,5 +15,13 @@ wget -N https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/r
 
 ls -lrth
 
-cd ../../code
+cd ../imdb/parts
+cp --backup=numbered *.dat backup/.
+#hopefully no lock files
+rm -f movies*.dat
+
+cd ../../../code
+
+python split_movies_list.py 
+
 
