fe56b27f34fa5a7ee5cffa3aa3f45034a7471a0b
saikatgomes <saikatgomes@gmail.com>
2014-10-30 18:05:29 -0500

    get advanced ratings

--- code/imdb_crawl.py
+++ code/imdb_crawl.py
@@ -8,6 +8,8 @@ BASE_URL="http://imdb.com"
 DATA_DIR="../data/tweet"
 OUT_DIR="../data/imdb/parts"
 
+RATING_ONLY=0;
+
 C_COUNT=0
 M_COUNT=0
 
@@ -100,7 +102,57 @@ def process():
                     fail_file=DIR+"/movies_"+f_num+".fail"
                     with open(fail_file,"w") as fl:
                         fl.write("Failed at "+socket.gethostname())
+                        fl.write("ERROR:")
+                        fl.write(e)
                 os.remove(lockfile)
+
+def get_rating_info(id,err_file,rating,rating_count):
+    rating_info=[]
+    url="http://www.imdb.com/title/tt"+id+"/ratings?"
+    print(getTime()+"\tCrawling url: "+url)
+    main_page=requests.get(url)
+    tree = html.fromstring(main_page.text)
+    aLine=get_value_list(tree,"//div[@id='tn15content']/p/text()",err_file,id,"line1",0)
+    line1=aLine[0]
+    if (line1.startswith("No breakdown available for IMDb users.")):
+       print(getTime()+"\t\tNo Ratings info avaiable")
+       err_file.write(getTime()+"ID:"+id+"|MSG:No_RATINGS_Found\n")
+    else:
+       x=line1.find("IMDb")
+       usr_count=line1[:x-1]
+       r_count=rating_count.replace(',','')
+       d=tree.xpath("//div[@id='tn15content']/table")
+       t1=d[0]
+       t2=d[1]
+       #demo breakdown
+       d1=t1.xpath("tr")
+       d2=t2.xpath("tr")
+       rating_value=[]
+       rating_report=[]
+
+       for X in range (1,len(d1)):
+           n=d1[X].xpath("td/text()")
+           rating_value.append({'value':n[2],'count':n[0]});
+       Z=len(d2)-2
+       for Y in range (1,len(d2)):
+           if Y!=Z:           
+               tb=d2[Y].xpath('td')
+               if Y==(Z+1):
+                   lbl=tb[0].xpath('text()')[0]
+                   if lbl.startswith(" IMDb users"):
+                       lbl="IMDb users"
+                   else:
+                       print(getTime()+"\t\t'IMDb users' expected but got '"+lbl+"'")
+                       err_file.write(getTime()+"ID:"+id+"|MSG:No_IMDb_Users_Found\n")
+               else:
+                   lbl=tb[0].xpath('a/text()')[0]
+               vote=tb[1].xpath('text()')[0]
+               ave=tb[2].xpath('text()')[0]
+               rating_report.append({'type':lbl,'votes':vote[1:],'average':ave[1:]})
+
+       rating_info.append({'rating':rating,'rating_count':usr_count, \
+                        'url':url,'ratings_breakdown':rating_value,'demo_breakdown':rating_report})
+    return rating_info 
 
 
 def get_movie_url(aFile,out_file):
@@ -119,6 +171,7 @@ def get_movie_url(aFile,out_file):
                 title=myData[1][:len(myData[1])-7]
                 year=myData[1][len(myData[1])-5:len(myData[1])-1]
                 cat=re.split('\|',myData[2][:len(myData[2])-1])
+                print(getTime()+"Getting Info for ID="+id+" TITLE="+title+" YEAR="+year)
                 # URLs
                 find_url=BASE_URL+"/find?ref_=nv_sr_fn&q="+title.replace(' ','+')+"&s=tt"
                 find_url2=BASE_URL+"/find?q="+title.replace(' ','+')+"&s=tt"
@@ -126,7 +179,6 @@ def get_movie_url(aFile,out_file):
                 main_url=BASE_URL+"/title/tt"+id+"/"
                 main_page=requests.get(main_url)
                 tree = html.fromstring(main_page.text)
-                print(getTime()+"Crawling url: "+main_url)
                 print(getTime()+"\t\tTweet Data:")
                 print(getTime()+"\t\t\tID: "+id)
                 print(getTime()+"\t\t\tTitle: "+title)
@@ -134,8 +186,11 @@ def get_movie_url(aFile,out_file):
                 msg=", ".join(cat)
                 print(getTime()+"\t\t\tGenre: "+msg)
                 # SCRAP FOR DATA
+                print(getTime()+"\tCrawling url: "+main_url)
                 print(getTime()+"\t\tIMDB Data:")
                 rating = get_value(tree,"//span[@itemprop='ratingValue']/text()",err_file,id,"Ratings")
+                rating_count = get_value(tree,"//span[@itemprop='ratingCount']/text()",err_file,id,"Ratings_Count")
+                #rating_count = rating_count.repace(',','')
                 director = get_value_list(tree,"//div[@itemprop='director']/a/span[@itemprop='name']/text()",err_file,id,"Director")
                 stars=get_value_list(tree,"//div[@itemprop='actors']/a/span[@itemprop='name']/text()",err_file,id,"Actors")
                 recs=get_value_list(tree,"//div[starts-with(@class,'rec_item')]/@data-tconst",err_file,id,"Recs")
@@ -180,13 +235,15 @@ def get_movie_url(aFile,out_file):
                 print(getTime()+"\t\t\tKnow As: "+aka)
                 msg = ", ".join(prod_co)
                 print(getTime()+"\t\t\tProd Co: "+msg)
+                #RATINGS
+                rating_info=get_rating_info(id,err_file,rating,rating_count)
                 # STORE!
                 MOVIES.append({\
                             'id':id,'title_tweet':title,'year_tweet':year,'genre_tweet':cat,'url':main_url, \
                             'rating_imdb':rating,'director_imdb':director,'actors_imdb':stars,'recs_imdb':recs, \
                             'mpaa_imdb':mpaa,'keywords':keywords,'language_imdb':lang,'color_imdb':color, \
                             'runtime_imdb':runtime,'budget_imdb':budget,'gross_imdb':gross,'also-known-as_imdb':aka, \
-                            'release_date_imdb':rel_date,'production_co_imdb':prod_co \
+                            'release_date_imdb':rel_date,'production_co_imdb':prod_co,'ratings_info_imdb':rating_info \
                             })
                 if M_COUNT%3==0:
                     time.sleep(1)
